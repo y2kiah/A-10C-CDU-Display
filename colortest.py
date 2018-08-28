f95fd8c5cb91a4f9cbd250993e61f38737c97462
y2kiah <y2kiah@hotmail.com>
2018-08-27 23:17:58 -0400

    fixed colortest

--- colortest.py
+++ colortest.py
@@ -12,16 +12,17 @@ import curses
 def main(stdscr):
 	curses.start_color()
 	curses.use_default_colors()
+	stdscr.scrollok(True)
 
 	stdscr.addstr("can change " if curses.can_change_color() else "cannot change ")
-	stdscr.addstr(str(curses.COLORS))
+	stdscr.addstr(str(curses.COLORS) + " ")
 
 	for i in range(0, curses.COLORS):
 		curses.init_pair(i + 1, i, -1)
 	try:
 		for i in range(0, 256):
 			stdscr.addstr(str(i), curses.color_pair(i))
-	except curses.ERR:
+	except:
 		# End of screen reached
 		pass
 
